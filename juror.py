895939d7be4fb3e765bcfb1eccb583a9efdcd9a6
Zared Shawver <zshawver@gmail.com>
2025-01-21 06:40:08 -0500

    Add attributes for predictions to Juror class

--- juror.py
+++ juror.py
@@ -3,5 +3,7 @@
 class Juror():
     def __init__(self,name, iv_dict):
         self.name = name
+        self.predictions = []
+        self.prediction = None
         for iv_name, value in iv_dict.items():
             setattr(self, iv_name, value)
