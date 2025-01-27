0943bda19b5214b84be4b29d702d2f7973c85b9e
Zared Shawver <zshawver@gmail.com>
2025-01-27 15:41:10 -0500

    Input actual results and juror files into sandbox to test small-scale predictions

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 from itertools import combinations
+import numpy as np
+
 
 # Generate all combinations for 1-IV, 2-IV, and 3-IV tests
 def generate_combinations(batch, max_comb=3):
@@ -40,7 +42,7 @@ def filter_results_by_combinations(df, combinations):
     # Concatenate all matching rows into a single DataFrame
     return pd.concat(filtered_results, ignore_index=True)
 
-
+chi_square_results_FL = pd.read_excel('C:/Users/zshawver/OneDrive - Dubin Consulting/Profile Testing/Data/FLOpioidsFG_Results_WeightTesting.xlsx',sheet_name='results')
 
 chi_square_results = pd.DataFrame({
     'IV': ['Age_30Split','Age_30Split','Age_30Split','Age_30Split','Age_30Split','Age_30Split','Age_30Split','Age_30Split'],
@@ -74,47 +76,32 @@ print(chi_square_results.head())
 
 print("Original Results Shape:", chi_square_results.shape)
 
-
-
-import pandas as pd
-import numpy as np
-
 # Example juror_data
 juror_data = pd.DataFrame({
-    'juror_name': ['John S', 'Bob R', 'Mike T', 'James W', 'Will F', 'Carl W'],
-    'DV_1PL_0Def': ['Plaintiff', 'Plaintiff', 'Defense', 'Plaintiff', 'Defense', 'Defense'],
-    'GENDER': ['Gender: Male', 'Gender: Male', 'Gender: Female', 'Gender: Male', 'Gender: Female', 'Gender: Female'],
-    'RACE': ['Race: White / Caucasian', 'Race: White / Caucasian', 'Race: White / Caucasian',
-             'Race: Black / African American', 'Race: White / Caucasian', 'Race: White / Caucasian'],
-    'PA': ['Political Affiliation: Democrat', 'Political Affiliation: Democrat',
-           'Political Affiliation: Republican', 'Political Affiliation: Democrat',
-           'Political Affiliation: Republican', 'Political Affiliation: Not Registered'],
-    'SOCIAL_CONS': ['Liberal / progressive', 'Liberal / progressive', 'Moderate',
-                    'Moderate', 'Moderate', 'Moderate'],
-    'MARITAL_STATUS': ['Marital Status: Married', 'Marital Status: Engaged',
-                       'Marital Status: Living with partner', 'Marital Status: Married',
-                       'Marital Status: Married', 'Marital Status: Separated or divorced'],
-    'AGE_RANGE': ['Age: 40-49', 'Age: 30-39', 'Age: 20-29', 'Age: 60-69', 'Age: 50-59', 'Age: 40-49'],
-    'Age_30Split': ['Age Greater than 30', 'Age Greater than 30', 'Age Less Than 30',
-                    'Age Greater than 30', 'Age Greater than 30', 'Age Greater than 30'],
-    'Age_40Split': ['Age Greater than 40', 'Age Less Than 40', 'Age Less Than 40',
-                    'Age Greater than 40', 'Age Greater than 40', 'Age Greater than 40'],
-    'Age_50Split': ['Age Less Than 50', 'Age Less Than 50', 'Age Less Than 50',
-                    'Age Greater than 50', 'Age Greater than 50', 'Age Less Than 50'],
-    'Age_60Split': ['Age Less Than 60', 'Age Less Than 60', 'Age Less Than 60',
-                    'Age Greater than 60', 'Age Less Than 60', 'Age Less Than 60']
+    'juror_name': ['John S','Bob R','Mike T','James W','Will F','Carl W','Tina T','Laura L','Candice B','Amy P','Rachel D','Seth M','Adam S','Jason B','Kristen W'],
+    'DV_1PL_0Def': ['Defense','Plaintiff','Plaintiff','Defense','Defense','Plaintiff','Defense','Plaintiff','Defense','Plaintiff','Defense','Defense','Plaintiff','Defense','Plaintiff'],
+    'GENDER': ['Gender: Man','Gender: Woman','Gender: Man','Gender: Woman','Gender: Man','Gender: Man','Gender: Man','Gender: Man','Gender: Woman','Gender: Man','Gender: Woman','Gender: Woman','Gender: Woman','Gender: Man','Gender: Woman'],
+    'RACE': ['Race: White / Caucasian','Race: White / Caucasian','Race: White / Caucasian','Race: Black / African American','Race: White / Caucasian','Race: Hispanic / Latinx','Race: White / Caucasian','Race: Hispanic / Latinx','Race: Black / African American','Race: Black / African American','Race: White / Caucasian','Race: White / Caucasian','Race: Black / African American','Race: Black / African American','Race: Black / African American'],
+    'PA': ['Political Affiliation: Democrat','Political Affiliation: Not Registered / Unaffiliated / Other','Political Affiliation: Democrat','Political Affiliation: Not Registered / Unaffiliated / Other','Political Affiliation: Democrat','Political Affiliation: Independent','Political Affiliation: Democrat','Political Affiliation: Republican','Political Affiliation: Democrat','Political Affiliation: Democrat','Political Affiliation: Republican','Political Affiliation: Democrat','Political Affiliation: Democrat','Political Affiliation: Independent','Political Affiliation: Democrat'],
+    'AGE_RANGE': ['Age: 50-59','Age: 30-39','Age: 40-49','Age: 20-29','Age: 60-69','Age: 30-39','Age: 60-69','Age: 50-59','Age: 40-49','Age: 60-69','Age: 60-69','Age: 70+','Age: 60-69','Age: 60-69','Age: 50-59'],
+    'Age_30Split': ['Age Greater than 30','Age Greater than 30','Age Greater than 30','Age Less Than 30','Age Greater than 30','Age Greater than 30','Age Greater than 30','Age Greater than 30','Age Greater than 30','Age Greater than 30','Age Greater than 30','Age Greater than 30','Age Greater than 30','Age Greater than 30','Age Greater than 30'],
+    'Age_40Split': ['Age Greater than 40','Age Less Than 40','Age Greater than 40','Age Less Than 40','Age Greater than 40','Age Less Than 40','Age Greater than 40','Age Greater than 40','Age Greater than 40','Age Greater than 40','Age Greater than 40','Age Greater than 40','Age Greater than 40','Age Greater than 40','Age Greater than 40'],
+    'Age_50Split': ['Age Greater than 50','Age Less Than 50','Age Less Than 50','Age Less Than 50','Age Greater than 50','Age Less Than 50','Age Greater than 50','Age Greater than 50','Age Less Than 50','Age Greater than 50','Age Greater than 50','Age Greater than 50','Age Greater than 50','Age Greater than 50','Age Greater than 50'],
+    'Age_60Split': ['Age Less Than 60','Age Less Than 60','Age Less Than 60','Age Less Than 60','Age Greater than 60','Age Less Than 60','Age Greater than 60','Age Less Than 60','Age Less Than 60','Age Greater than 60','Age Greater than 60','Age Greater than 60','Age Greater than 60','Age Greater than 60','Age Less Than 60'],
+    'LC__PROFIT_COMPARE_3': ['Belives that Walmart Is more profitable than other corporations','Belives that Walmart Is more profitable than other corporations','Belives that Walmart Is equally as profitable as other corporations','Belives that Walmart Is more profitable than other corporations','BLANK','Belives that Walmart Is more profitable than other corporations','Belives that Walmart Is more profitable than other corporations','Belives that Walmart Is more profitable than other corporations','Belives that Walmart Is more profitable than other corporations','Belives that Walmart Is more profitable than other corporations','Belives that Walmart Is equally as profitable as other corporations','BLANK','BLANK','BLANK','Belives that Walmart Is equally as profitable as other corporations']
+
 })
 
-# Example filtered_results
-filtered_results = pd.DataFrame({
-    'IV': ['Age_30Split', 'Age_40Split', 'Age_50Split'],
-    'Control_1': ['Age_50Split', 'Age_50Split', 'Age_60Split'],
-    'Control_2': ['Age_40Split', np.nan, 'Age_30Split'],
-    'IVLabel': ['Age Greater than 30', 'Age Greater than 40', 'Age Less Than 50'],
-    'Control_1_Label': ['Age Less Than 50', 'Age Greater than 50', 'Age Less Than 60'],
-    'Control_2_Label': ['Age Greater than 40', np.nan, 'Age Greater than 30'],
-    'weighted_prediction': [0.12, 0.15, 0.08]
-})
+# # Example filtered_results
+# filtered_results = pd.DataFrame({
+#     'IV': ['Age_30Split', 'Age_40Split', 'Age_50Split'],
+#     'Control_1': ['Age_50Split', 'Age_50Split', 'Age_60Split'],
+#     'Control_2': ['Age_40Split', np.nan, 'Age_30Split'],
+#     'IVLabel': ['Age Greater than 30', 'Age Greater than 40', 'Age Less Than 50'],
+#     'Control_1_Label': ['Age Less Than 50', 'Age Greater than 50', 'Age Less Than 60'],
+#     'Control_2_Label': ['Age Greater than 40', np.nan, 'Age Greater than 30'],
+#     'weighted_prediction': [0.12, 0.15, 0.08]
+# })
 
 # Function to match juror responses with filtered results
 def match_jurors(juror_data, filtered_results):
@@ -137,12 +124,25 @@ def match_jurors(juror_data, filtered_results):
         matched_jurors = juror_data[condition]
         for _, juror in matched_jurors.iterrows():
             results.append({
-                'juror_name': juror['juror_name'],
+                'NAME': juror['NAME'],
                 'weighted_prediction': prediction
             })
 
     return pd.DataFrame(results)
 
+juror_data_FL = pd.read_excel('C:/Users/zshawver/OneDrive - Dubin Consulting/Profile Testing/Data/FL_Opioids_JurorData.xlsx',sheet_name='use-labels')
+
 # Apply function
-matched_results = match_jurors(juror_data, filtered_results)
+matched_results = match_jurors(juror_data_FL, filtered_results)
 print(matched_results)
+
+for _,row in matched_results.iterrows():
+    print(row['NAME'],row['weighted_prediction'])
+
+for _,row in juror_data_FL.iterrows():
+    if row['NAME'] in matched_results['NAME'].values:
+        match_index = matched_results[matched_results['NAME'] == row['NAME']].index[0]
+        # Pull a value from another column using that index
+        prediction = matched_results.loc[match_index, 'weighted_prediction']
+        outcome = row['DV_1PL_0Def']
+        print(f"{row['NAME']}, predicted {prediction} %PL was {outcome}")
